1af63cfe2f1253945a7229fbe517cc6bc5f7facc
Brandi <blubrandi0@gmail.com>
2020-02-19 20:33:59 -0700

    Map w/ LongPress
    Map is correctly rendering map coords on LongPress, and updates with each seceding LongPress.

--- GeoSeekPlayProject/ViewController.swift
+++ GeoSeekPlayProject/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
     
     @IBOutlet weak var myMapView: MGLMapView!
     
@@ -24,33 +24,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setMapView()
-        
-    }
-    
-    func setMapView() {
         let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStyleURL)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 33.812794, longitude: -117.9190981), zoomLevel: 13, animated: false)
-        
-        view.addSubview(mapView)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 33.812794, longitude: -117.9190981), zoomLevel: 15, animated: false)
         
         let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
         lpgr.minimumPressDuration = 0.5
         lpgr.delaysTouchesBegan = false
         mapView.addGestureRecognizer(lpgr)
+        
+        view.addSubview(mapView)
+        
     }
     
     //MARK: - UILongPressGestureRecognizer Action -
     @objc func handleLongPress(gestureReconizer: UILongPressGestureRecognizer) {
         if gestureReconizer.state != UIGestureRecognizer.State.ended {
-            //When lognpress is start or running
         } else {
             print("I was long pressed...")
             
-            let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStyleURL)
-            let touchPoint = gestureReconizer.location(in: mapView)
-            let coordsFromTouchPoint = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            myMapView.setCenter(CLLocationCoordinate2D(latitude: 33.812794, longitude: -117.9190981), zoomLevel: 15, animated: false)
+            let touchPoint = gestureReconizer.location(in: myMapView)
+            let coordsFromTouchPoint = myMapView.convert(touchPoint, toCoordinateFrom: myMapView)
             pressedLocation = CLLocation(latitude: coordsFromTouchPoint.latitude, longitude: coordsFromTouchPoint.longitude)
             print("Location:", coordsFromTouchPoint.latitude, coordsFromTouchPoint.longitude)
             
